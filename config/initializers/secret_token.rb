f90db7ec9a46bcebafafe49ad72c91f12a8ece98
Sergio Revilla <sergio@unmundotecnologico.org>
2014-02-06 22:26:35 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PopUpStores::Application.config.secret_key_base = '19ff6744aa17caad5d117798a6b90f7b17626ffda6a9cd4e78d2aa664d0bb37e25db12bd4fa2905e082b577fe45b5beae1c9d9c214936aefca70f3d245c2dc72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PopUpStores::Application.config.session_store :cookie_store, key: '_pop-up-stores_session'
