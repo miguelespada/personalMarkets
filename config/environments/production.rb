f9dd98a6dcfc15d6b6026d4c83fa7280e5fe0b81
Sergio Revilla <sergio@unmundotecnologico.org>
2014-02-11 21:25:41 +0100

    Serving static assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ PopUpStores::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,7 +29,6 @@ PopUpStores::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.initialize_on_precompile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
