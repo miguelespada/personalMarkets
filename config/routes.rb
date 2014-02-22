a75a5db90a1a0208da563cc658d794ad9c9715c7
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-02-22 22:12:41 +0100

    add create market form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ PopUpStores::Application.routes.draw do
   devise_for :users
 
   get 'markets' => 'markets#index'
+  get 'markets/edit' => 'markets#edit'
    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
