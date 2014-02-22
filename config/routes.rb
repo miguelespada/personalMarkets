8d337099e647d5b645633b2f8a5601f65e464d71
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-02-22 21:44:12 +0100

    simple navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ PopUpStores::Application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root "static_pages#home"
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  get 'markets' => 'markets#index'
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
