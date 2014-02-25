99b60b3c2c201bd42c12a5abf28fa272225f2475
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-02-25 12:38:39 +0100

    Add test in controller to upload empty no photo

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -22,7 +22,9 @@ class MarketsController < ApplicationController
 
   def update
     @market = Market.find(params[:id])
-    @market.update_attribute(:featured, market_params[:featured])
+    if market_params[:featured]
+      @market.update_attribute(:featured, market_params[:featured])
+    end
   end
 
   private
--- spec/controllers/markets_controller_spec.rb
+++ spec/controllers/markets_controller_spec.rb
@@ -51,16 +51,29 @@ describe MarketsController do
 
 
       describe "updload foto" do
-        it "assigns a newly created market" do
-          market.stub(:featured).and_return('[{"public_id":"dummy","version":1,"signature":"dummy",
-            "width":75,"height":75,"format":"png","resource_type":"image","created_at":"dummy_data",
-            "tags":["attachinary_tmp","development_env"],"bytes":0,"type":"upload",
+        it "uploads a valid featured photo" do
+          market.stub(:featured).and_return('[{"public_id":"dummy",
+            "version":1,
+            "signature":"dummy",
+            "width":75,
+            "height":75,
+            "format":"png",
+            "resource_type":"image",
+            "created_at":"dummy_data",
+            "tags":["attachinary_tmp","development_env"],
+            "bytes":0,
+            "type":"upload",
             "etag":"dummy_etag",
-            "url":"dummy.png",
-            "secure_url":"dummy.png"}]')
+            "url":"http://dummy.png",
+            "secure_url":"http://dummy.png"}]')
 
           put :update, {id: market.to_param, market: market.attributes}, valid_session
           expect(market.featured).not_to be_nil
+        end
+    
+        it "uploads an empty featured photo" do
+          put :update, {id: market.to_param, market: market.attributes}, valid_session          
+          expect(market.featured).to be_nil
         end
     end
 
