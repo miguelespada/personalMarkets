5968789e7479c97610173eed46635a0b0e2d35cc
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-02-25 12:59:59 +0100

    Add flash notice when photo succesfully saved

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -22,8 +22,10 @@ class MarketsController < ApplicationController
 
   def update
     @market = Market.find(params[:id])
-    if market_params[:featured] 
+    
+    if market_params[:featured] and market_params[:featured] != ""
       @market.update_attribute(:featured, market_params[:featured])
+      flash[:notice] = "Featured photo successfully saved!"
     end
 
   end
