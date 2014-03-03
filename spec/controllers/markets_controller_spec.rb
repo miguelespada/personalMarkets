c875a935bf2a2123f7e2fadbed6d46bb66fe7993
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-03 11:46:03 +0100

    Specs for search with elastic search

--- spec/controllers/markets_controller_spec.rb
+++ spec/controllers/markets_controller_spec.rb
@@ -116,19 +116,24 @@ describe MarketsController do
         Market.es.index.delete
         Market.es.index.exists?.should be_false
       end
-      context 'searching' do
-        before :each do
+      it 'searches and returns models' do
+          Market.es.index.delete
           @m1 = FactoryGirl.create(:market)
+          @m1.update_attribute(:name, "Test")
           @m2 = FactoryGirl.create(:market)
           @m3 = FactoryGirl.create(:market)
+          puts Market.all.to_json
+          puts Market.all.count
           Market.es.index.refresh
-        end
-        it 'searches and returns models' do
           results = Market.es.search q: 'Market'
           results.count.should eq 3
           results.to_a.count.should eq 3
+          results = Market.es.search q: @m1.name
+          results.count.should eq 1
+          results.to_a.count.should eq 1
           results.first.id.should eq @m1.id
-        end
+          results.first.user_id.should eq @m1.user_id
+      end
   end
 
 end
