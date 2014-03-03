0cae1bdeb9f2642d128e3d42c0cb2d7938b4e679
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-03 11:23:35 +0100

    Specs for create and delete index

--- spec/controllers/markets_controller_spec.rb
+++ spec/controllers/markets_controller_spec.rb
@@ -119,6 +119,20 @@ describe MarketsController do
         Market.es.index.exists?.should be_false
       end
     end
+    context 'searching' do
+     before :each do
+        @m1 = FactoryGirl.create(:market)
+        @m2 = FactoryGirl.create(:market)
+        @m3 = FactoryGirl.create(:market)
+        MARKET.es.index.refresh
+      end
+     it 'searches and returns models' do
+      results = Market.es.search q: 'Market'
+      results.count.should eq 3
+      results.to_a.count.should eq 3
+      results.first.id.should eq @m1.id
+    end
+    end
   end
 
 end
