71b4f917db0f20f2ff47ca9f51f56f07aa953b58
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-04 10:50:51 +0100

    cleaning factories

--- spec/factories/markets.rb
+++ spec/factories/markets.rb
@@ -7,24 +7,4 @@ FactoryGirl.define do
     user
     category
   end
-  
-  factory :market_with_photo, class: Market do
-    name "My personal market"
-    description "My market description"
-    featured '[{"public_id":"dummy",
-            "version":1,
-            "signature":"dummy",
-            "width":75,
-            "height":75,
-            "format":"png",
-            "resource_type":"image",
-            "created_at":"dummy_data",
-            "tags":["attachinary_tmp","development_env"],
-            "bytes":0,
-            "type":"upload",
-            "etag":"dummy_etag",
-            "url":"http://dummy.png",
-            "secure_url":"http://dummy.png"}]'
-  end
-  
 end
