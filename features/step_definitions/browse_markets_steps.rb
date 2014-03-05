2483fca8df12680bda8b46f426fdf3c42457dea1
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-05 21:08:44 +0100

    Features refactored

--- features/step_definitions/browse_markets_steps.rb
+++ features/step_definitions/browse_markets_steps.rb
@@ -36,9 +36,36 @@ When(/^I click the details link of a market$/) do
 end
 
 Then(/^I should see the full description of a market$/) do
-
   expect(page).to have_content @market0.name
   expect(page).to have_content @market0.description
   expect(page).to have_content @market0.category.name
   expect(page).to have_content @market0.user.email
 end
+
+Given(/^I am a signed in$/) do
+  @user = FactoryGirl.create(:user)
+
+  click_link "Sign in"
+  fill_in "Email",  with: @user.email
+  fill_in "Password",  with: "password", :match => :prefer_exact
+  click_button "Sign in"
+
+  FactoryGirl.create(:market, :name => "My market one", :user => @user)
+  FactoryGirl.create(:market, :name => "My market two", :user => @user)
+end
+
+Given(/^I go to my markets$/) do
+  visit user_markets_path(@user)
+end
+
+Then(/^I should see all my markets$/) do
+  expect(page).to have_content "My market one"
+  expect(page).to have_content "My market two"
+
+end
+
+Then(/^I should see only my markets$/) do
+  expect(page).not_to have_content "Market one"
+  expect(page).not_to have_content "Market two"
+end
+
