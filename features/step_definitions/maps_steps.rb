85e3f357966e69930bb1c510e111783489e5aea7
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-09 14:37:20 +0100

    remove tmp files

--- features/step_definitions/maps_steps.rb
+++
@@ -1,3 +0,0 @@
-Then(/^I should see "(.*?)"$/) do |arg1|
-      page.should have_content("Brooklyn")
-end
--- spec/requests/maps_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-  describe "check maps" do
-    it "Js working",  :js => true do
-      visit "/"
-      page.should have_content("Brooklyn")
-    end
-  end
