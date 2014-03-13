bb91de802b1b312bb1ae2e405b99e629ebb0ac56
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-13 14:48:54 +0100

    add user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,30 @@ require 'spec_helper'
 describe UsersController do
 	
  let(:valid_session) { {} }
+ let(:user) { FactoryGirl.create(:user) } 
+let(:market) { FactoryGirl.create(:market)}
+
  it "renders the index template" do
       get :index, valid_session
       expect(response).to render_template("index")
+ end
+
+  describe "favorite" do
+   it "render index" do
+        get :favorite,{ user_id: user.to_param, market_id: market.to_param}, valid_session
+        expect(response).to render_template("show")
+   end
+
+   it "adds like" do
+        get :favorite,{ user_id: user.to_param, market_id: market.to_param, v: true}, valid_session
+        change(user.favorites, :count).by(1)
+        change(market.favorited, :count).by(1)
+   end
+
+   it "remove like" do
+        get :favorite,{ user_id: user.to_param, market_id: market.to_param, v: false}, valid_session
+        change(user.favorites, :count).by(-1)
+        change(market.favorited, :count).by(-1)
+   end
  end
 end
