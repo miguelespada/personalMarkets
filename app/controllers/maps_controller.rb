433b069395e41b02dab14cf8d461a8d60d230e78
jameshedaweng <heda.weng@gmail.com>
2014-03-14 17:54:16 +0100

    add google maps support

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,6 +4,7 @@ class MapsController < ApplicationController
 		@hash = Gmaps4rails.build_markers(@markets) do |market, marker|
   		marker.lat market.latitude
   		marker.lng market.longitude
+  		marker.infowindow render_to_string(:partial => "/markets/shared/tooltip", :locals => { :market => market})
 		end
 	end
 end
