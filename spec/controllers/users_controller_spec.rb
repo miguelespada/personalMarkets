7a4a0ac5f956bd69803130aa1b3962d0790eed43
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-14 08:50:35 +0100

    Controller specs for like

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,7 +11,7 @@ describe UsersController do
       expect(response).to render_template("index")
  end
 
-  describe "favorite" do
+  describe "Like" do
    it "render index" do
         get :favorite,{ user_id: user.to_param, market_id: market.to_param}, valid_session
         expect(response).to render_template("show")
@@ -30,11 +30,26 @@ describe UsersController do
         market.favorited.count.should eq 1
         market.favorited.include?(assigns(:user)).should eq true
    end
-   
-   describe "unlike" do
+
+   it "user unlikes a market" do
+        market.favorited.count.should eq 0
+        get :favorite,{ user_id: user.to_param, market_id: market.to_param, :v => 0}, valid_session
+        market.reload
+        market.favorited.count.should eq 0
+        market.favorited.include?(assigns(:user)).should eq false
+   end
+
+   describe "After a like" do
       before :each do
         user.like(market, 1)
       end
+      it "user likes market twice" do
+        user.favorites.count.should eq 1
+        get :favorite,{ user_id: user.to_param, market_id: market.to_param, :v => 1}, valid_session
+        assigns(:user).favorites.count.should eq 1
+        assigns(:user).favorites.include?(market).should eq true
+      end
+
       it "user unlikes market" do
         user.favorites.count.should eq 1
         get :favorite,{ user_id: user.to_param, market_id: market.to_param, :v => 0}, valid_session
