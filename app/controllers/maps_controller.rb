3d40ca6fd75fe7db7a9e80f12d6df42a38494c28
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-15 09:30:22 +0100

    add condition for markers

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,10 +1,11 @@
 class MapsController < ApplicationController
-	def index
-		@markets = Market.all
-		@hash = Gmaps4rails.build_markers(@markets) do |market, marker|
-  		marker.lat market.latitude
-  		marker.lng market.longitude
-  		marker.infowindow render_to_string(:partial => "/markets/shared/tooltip", :locals => { :market => market})
-		end
-	end
+  def index
+    @markets = Market.find({"latitude":{$exists:true}, {"longitude":{$exists:true})
+    @hash = Gmaps4rails.build_markers(@markets) do |market, marker|
+        marker.lat market.latitude
+        marker.lng market.longitude
+        marker.infowindow render_to_string(:partial => "/markets/shared/tooltip", 
+                         :locals => { :market => market})
+    end
+  end
 end
