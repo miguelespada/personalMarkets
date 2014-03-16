f2a246ecd4d72cb4e9d688ea13cb87fba7db60a2
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-16 20:28:57 +0100

    Recofigured attachinary

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -1,5 +1,11 @@
 //= require tagmanager.js
+//= require jquery.ui.widget
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 
 $( document ).ready(function() {
   $(".tm-input").tagsManager({prefilled: $( "form" ).data( "tags")});
+  $('.attachinary-input').attachinary()
 });
