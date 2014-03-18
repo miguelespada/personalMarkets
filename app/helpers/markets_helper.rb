2f9a8fb663d07773706c169392e94e28e01ff3f2
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-18 11:59:42 +0100

    refactor views using helpers, features passed

---
+++ app/helpers/markets_helper.rb
@@ -0,0 +1,43 @@
+module MarketsHelper
+  def full_description(market)
+    div_for market, class: "market-full-description" do
+      render market.decorate, :image_size => "200"
+    end
+  end
+
+  def gallery_item(market)
+    div_for market, class: "market-gallery-item" do
+      render market.decorate, :image_size => "100"
+    end
+  end
+
+  def slug_item(market)
+    div_for market, class: "market-slug" do
+      render market.decorate, :image_size => "50"
+    end
+  end
+
+  def tooltip(market)
+    div_for market, class: "market-tooltip" do
+      render market.decorate, :image_size => "50"
+    end
+  end
+
+  def gallery(markets, &block)
+      markets.collect{|market| concat(gallery_item(market))}
+      yield if block_given?
+  end
+
+  def slugs(markets)
+      markets.collect{|market| concat(slug_item(market))}
+      yield if block_given?
+  end
+
+  def market_form(user, market)
+    render partial: "markets/shared/form", locals: {user: user, market: market}
+  end
+
+  def search_box
+    render partial: "markets/shared/search"
+  end
+end
