37f3f0dafd460230ec5a9961427924e36764c432
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-18 17:43:49 +0100

    Basic cleaning

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -8,23 +8,13 @@
 var substringMatcher = function(strs) {
   return function findMatches(q, cb) {
     var matches, substringRegex;
- 
-    // an array that will be populated with substring matches
     matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
     substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
     $.each(strs, function(i, str) {
       if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
         matches.push({ value: str });
       }
     });
- 
     cb(matches);
   };
 };
@@ -33,12 +23,16 @@ var substringMatcher = function(strs) {
 $( document ).ready(function() {
    $('.attachinary-input').attachinary();
 
-   var tagApi = $(".tm-input").tagsManager({prefilled: $( "form" ).data( "tags")});
+   var tagApi = $(".tm-input").tagsManager({
+      prefilled: $( "form" ).data( "tags"),
+      delimiters: [13, 44]
+      });
   
   $.get( "/tags/index.json", function( data ) {
       $('.typeahead').typeahead({
         hint: true,
         highlight: true,
+
         minLength: 1
       },
       {
