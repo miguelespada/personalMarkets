07c0ad3c1240f009120bdbe1bf1772f7563ec071
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-19 21:38:56 +0100

    set calendar to start today

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -43,6 +43,16 @@ $( document ).ready(function() {
       });
   });
   
-  $('[data-behaviour~=datepicker]').datepicker();
+  $('[data-behaviour~=datepicker]').datepicker({
+
+      format: "dd/mm/yyyy",
+      startDate: "+0d",
+      endDate: "+30d",
+      todayHighlight: true,
+      todayBtn: true,
+      language: "es"
+    }
+
+    );
 });
 
