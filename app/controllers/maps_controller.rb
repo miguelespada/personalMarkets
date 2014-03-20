2d915a065a29f464f000c663b8f6197ec0896f04
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-20 14:55:05 +0100

    Add all marker specs

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,7 @@
 class MapsController < ApplicationController
   def index
-    markets = Market.where(:latitude.exists => true, :longitude.exists => true)
+    markets = Market.where(:latitude.nin => ["", nil], :longitude.nin => ["", nil])
+
     if markets.count > 0
       @geojson = markets.collect{|market| market.to_marker(view_context.tooltip(market))}
     end 
--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -18,5 +18,17 @@ describe MapsController do
       get 'index', valid_session
       expect(assigns(:geojson)).to eq ""
     end
+
+    it "marker empty location" do
+      FactoryGirl.create(:market, :latitude => "", :longitude => "")
+      get 'index', valid_session
+      expect(assigns(:geojson)).to eq ""
+    end
+
+    it "marker with location" do
+      FactoryGirl.create(:market, :latitude => "40", :longitude => "-3")
+      get 'index', valid_session
+      expect(assigns(:geojson)).not_to eq ""
+    end
   end
 end
