f8a7009026fe03c79b3b01fc7100210932ec09de
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-20 14:48:00 +0100

    Add marker spec for empty geo

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,11 +1,11 @@
 class MapsController < ApplicationController
   def index
-    markets = Market.all
+    markets = Market.where(:latitude.exists => true, :longitude.exists => true)
     if markets.count > 0
       @geojson = markets.collect{|market| market.to_marker(view_context.tooltip(market))}
     end 
     @geojson ||= ""
-    
+
     respond_to do |format|
       format.html 
       format.json {render json: @geojson}
