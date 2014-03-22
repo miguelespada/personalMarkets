76157df159529a66ccf7395ad4b115528b48f980
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-22 22:23:40 +0100

    add a shared steps file

---
+++ features/step_definitions/shared_steps.rb
@@ -0,0 +1,28 @@
+Given(/^I sign up$/) do
+  visit "/"
+  within(:css, ".user-session-menu") do
+    click_on "Sign up"
+  end
+  fill_in "Email",  with: "dummy@email.com"
+  fill_in "Password",  with: "password", :match => :prefer_exact
+  fill_in "Password confirmation",  with: "password"
+  within(:css, ".new_user") do
+    click_on "Sign up"
+  end
+end
+
+Given(/^I am a registerd user$/) do
+  @user = FactoryGirl.create(:user)
+end
+
+Given(/^I sign in$/) do
+  visit "/"
+  within(:css, ".user-session-menu") do
+    click_on "Sign in"
+  end
+  fill_in "Email",  with: @user.email
+  fill_in "Password",  with: @user.password, :match => :prefer_exact
+  within(:css, ".new_user") do
+    click_on "Sign in"
+  end
+end
