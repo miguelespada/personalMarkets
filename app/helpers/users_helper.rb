c05ea9556e514fac164656045970e5b693564bd6
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-23 10:26:33 +0100

    add user_likes helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def user_likes(user)
+    content_tag :div, class: "likes" do
+      render template: "markets/index",
+        :locals => {:markets => @user.favorites, :layout => "slugs"}
+    end
+  end
 end
