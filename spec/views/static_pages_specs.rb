19c97d8091833eb1107c97180f13f7ec4fdf4ffe
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-24 16:32:13 +0100

    Vendor assets removed, for clean start

--- spec/views/static_pages_specs.rb
+++ spec/views/static_pages_specs.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe "market" do
   xit "When I am logged I should see the user actions"
-  xit "When I am notlogged I should not see user actions"
+  xit "When I am not logged I should not see user actions"
 end
