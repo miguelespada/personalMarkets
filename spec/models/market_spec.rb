c0d2c92e066ffc56006e7a866f8eec0ba79e3980
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-24 21:19:25 +0100

    Add market model specs

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -3,10 +3,30 @@ require 'spec_helper'
 describe Market do
   it { should have_field :name }
   it { should have_field :description }
-  describe "Search with no index" do
+  describe "Search with no index" 
     it "creates an new index" do
       Market.delete_index
       expect{Market.search("", "")}.to change{Market.exists_index?}.from(false).to(true)
+    end
+  context "With no markets" do   
+    describe "search" do
+      it "returns an empty array" do
+        Market.destroy_all
+        expect(Market.search("", "")).to eq []
+      end
+    end
+  end
+  context "With markets" do
+    before :each do
+      @markets = []
+      10.times { @markets << FactoryGirl.create(:market)}
+      Market.refresh_index
+    end   
+    describe "search wit empty query" do
+      it "returns all the markets" do
+        result = Market.search("", "")
+        expect(result.count).to eq Market.all.count
+      end
     end
   end
 end
