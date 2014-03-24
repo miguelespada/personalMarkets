cb31f1fc545d5312be9074d4017988ac8a5a44e0
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-03-24 21:03:01 +0100

    Add market model spec

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -3,4 +3,10 @@ require 'spec_helper'
 describe Market do
   it { should have_field :name }
   it { should have_field :description }
+  describe "Search with no index" do
+    it "creates an new index" do
+      Market.delete_index
+      expect{Market.search("", "")}.to change{Market.exists_index?}.from(false).to(true)
+    end
+  end
 end
