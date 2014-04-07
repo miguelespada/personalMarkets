29e8999015080a37fb4d44f8fed7f9ea324ff629
Sergio Revilla <sergio@unmundotecnologico.org>
2014-04-07 14:41:41 +0200

    cancan abilities refactor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,39 +2,54 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    @user = user
 
-    if user
+    logged_in_abilities
+
+    @user ||= User.new
+
+    admin_abilities
+    moderator_abilities
+
+    can [:edit, :update], Comment, :author => @user.email
+    can :edit, Market, :user_id => @user.id
+    can :delete, Market, :user_id => @user.id
+  end
+
+
+  private
+
+  def logged_in_abilities
+    if @user
         can :comment, Market
         can :see_location, Market
         can :report, Comment
         can :like, Market, Market do |market|
-            user.can_like market
+            @user.can_like market
         end
         can :unlike, Market, Market do |market|
-            user.can_unlike market
+            @user.can_unlike market
         end
     end
+  end
 
-    user ||= User.new
-
-    if user.has_role? :admin
+  def admin_abilities
+    if @user.has_role? :admin
       can [:edit, :update], Comment
       can :destroy, Comment
       can :edit, Market
     end
+  end
 
-    if user.has_role? :moderator
+  def moderator_abilities
+    if @user.has_role? :moderator
       can :destroy, Comment
 
       can :delete_image, Market, Market do |market|
         market.featured?
       end
     else
-      can :destroy, Comment, :author => user.email
+      can :destroy, Comment, :author => @user.email
     end
-
-    can [:edit, :update], Comment, :author => user.email
-    can :edit, Market, :user_id => user.id
-    can :delete, Market, :user_id => user.id
   end
 end
