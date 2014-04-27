3519e84f38a79752c0e7b1d7bdb77cf24ac3871c
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-04-27 10:41:08 +0200

    refactores coupons

--- spec/abilities/users_abilities_spec.rb
+++ spec/abilities/users_abilities_spec.rb
@@ -1,24 +1,29 @@
 require 'spec_helper'
 require "cancan/matchers"
 
-describe "User" do
+describe "User" do      
+  
   describe "abilities" do
     subject(:ability){ Ability.new(user) }
     let(:other_user){ create(:user) }
-    
-    context "owner " do
+  
+    context "owner" do
       let(:user){ create(:user)}
       it{ should be_able_to(:manage, Wish.new(:user => user)) }
+      it{ should be_able_to(:buy, Coupon.new) }
+      it{ should be_able_to(:list_transactions, Coupon.new) }
     end
 
-    context "unauthorized " do
+    context "unauthorized" do
       let(:user){ create(:user)}
       it{ should_not be_able_to(:manage, Wish.new(:user => other_user)) }
     end
 
-    context "admin " do
+    context "admin" do
       let(:user){ create(:user, :admin)}
       it{ should be_able_to(:manage, Wish) }
+      it{ should be_able_to(:buy, Coupon.new) }
+      it{ should be_able_to(:list_transactions, Coupon.new) }
     end
   end
 end
