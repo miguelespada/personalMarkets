ed7460ccfc76ec5cc3c5cad570c01e0d4ea2aa01
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-05-14 16:36:50 +0200

    add simple spec for markets

--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -1,9 +1,6 @@
 FactoryGirl.define do
   factory :photo do    
     photo {{ public_id: "sxmltfqlramr1arsaaay", version: "1396518223", width: 259, height: 194, format: "jpg", resource_type: "image" }}
-    trait :with_market do
-        photographic
-    end
   end
 end
 
