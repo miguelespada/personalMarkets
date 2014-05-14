e8cf5f4e37ebe6de36d2fc10527ac7e8c131ddd1
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-05-14 14:28:26 +0200

    add markets authority specs

--- spec/controllers/markets_controller_permissions_spec.rb
+++ spec/controllers/markets_controller_permissions_spec.rb
@@ -11,6 +11,32 @@ describe MarketsController do
                   id: market.to_param } }
 
   describe "unauthorized user" do 
+
+    it "cannot index all" do
+      get :index, { user_id: user.to_param }, valid_session
+      expect(response.response_code).to eq 403
+    end
+
+
+    [:slideshow, :list_published_markets, :list_last_markets, 
+      :list_user_markets, :list_liked_markets, :search].each do |get_action|
+      it "can #{get_action}" do
+        get get_action, { user_id: user.to_param }, valid_session
+        expect(response.response_code).to eq 200
+      end
+    end
+    
+    it "can list by tag" do
+      get :list_tag_markets, { user_id: user.to_param, tag: "dummy" }, valid_session
+      expect(response.response_code).to eq 200
+    end
+
+    it "can list by category" do
+      category = create(:category)
+      get :list_category_markets, { user_id: user.to_param, category_id: category.to_param}, valid_session
+      expect(response.response_code).to eq 200
+    end
+
     it "cannot create" do
       post :create, market_params, valid_session
       expect(response.response_code).to eq 403
@@ -31,10 +57,6 @@ describe MarketsController do
         post post_action, { market_id: market.to_param }, valid_session
         expect(response.response_code).to eq 403
       end
-    end
-    it "cannot index all" do
-      get :index, { user_id: user.to_param }, valid_session
-      expect(response.response_code).to eq 403
     end
   end 
 
