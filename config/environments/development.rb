354035a74c80f764f97d9140c49c6457c0a263a3
olareoun <victor.miguez.martinez@gmail.com>
2014-05-23 13:59:25 +0200

    mail configuration for devise forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,20 @@ PopUpStores::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :domain => ENV['GMAIL_SMTP_USER'],
+    :user_name => ENV['GMAIL_SMTP_USER'],
+    :password => ENV['GMAIL_SMTP_PASSWORD']
+  }
 end
--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -5,8 +5,8 @@ Pony.options = {
     :address => 'smtp.gmail.com',
     :port => '587',
     :enable_starttls_auto => true,
-    :user_name => 'p3rs0n4l.m4rk3ts',
-    :password => 'magdalenasfederico',
+    :user_name => ENV['GMAIL_SMTP_USER'],
+    :password => ENV['GMAIL_SMTP_PASSWORD'],
     :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
