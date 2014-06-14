367e5b6ce133f054113f4169aa3c0d43816995d0
miguelespada <miguel.valero.espada@gmail.com>
2014-06-14 13:07:03 +0200

    removing market helper wip

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -7,30 +7,4 @@ module MarketsHelper
              :locals => {:market => market.decorate}
     end
   end
-
-  def market_form(user, market)
-    if user.allowed_market_creation? 
-      render partial: "markets/shared/form", locals: {user: user, market: market.decorate }
-    else
-      message = "You have to wait one month to create another market"
-      render partial: "markets/shared/error", locals: { message: message }
-    end
-  end
-
-  def edit_market_form(user, market)
-    render partial: "markets/shared/form", locals: {user: user, market: market.decorate }
-  end
-
-  def search_box
-    render partial: "markets/shared/search"
-  end
-
-  def likes(market, &block)
-    market.favorited.collect{|user| concat(user.email + " ")}
-    yield if block_given?
-  end
-
-  def are_more_markets?(markets)
-    "hidden" if markets == []
-  end
 end
