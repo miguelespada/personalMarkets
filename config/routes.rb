5df2fff120484f86b53ad32b7f50a47f6d126937
miguelespada <miguel.valero.espada@gmail.com>
2014-06-30 14:08:24 +0200

    add vim

--- config/routes.rb
+++ config/routes.rb
@@ -136,6 +136,7 @@ PopUpStores::Application.routes.draw do
     get "static_pages/buy_coupon_terms", as: "buy_coupon_terms"
     get "static_pages/sell_coupon_terms", as: "sell_coupon_terms"
     get "static_pages/go_premium_terms", as: "go_premium_terms"
+    get "static_pages/go_vim_terms", as: "go_vim_terms"
     get "static_pages/privacy", as: "privacy"
     get "static_pages/settings", path: "/settings", as: "settings"
 
