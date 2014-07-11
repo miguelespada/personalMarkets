4d1e65b450b9e399d27183b638bc6b595d174217
jameshedaweng <heda.weng@gmail.com>
2014-07-11 16:16:06 +0800

    category buttons restyle

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -1,24 +1,24 @@
 module CategoryHelper
   def formatted_button(category)
     "<button type='button' 
-        class='theme theme-styled btn btn-sm category-theme-button' 
-        style='background-color:#{category.color}' 
+        class='theme theme-styled btn btn-sm category-theme-button btn-category' 
+        style='background-color:#FFFFFF' 
         tile='#{category.style}'
         id='#{category.slug}'
         title='#{category.slug}'>
         <i class='fa #{category.glyph}'>
-        </i> #{intl_name(category)}
+        </i> <span>#{intl_name(category)}</span>
     </button> ".html_safe
   end
   
   def all_categories_button
       "<button type='button' 
-          class='theme btn btn-sm btn-all-categories' 
+          class='theme btn btn-sm btn-all-categories btn-category' 
           style='background-color:#FFFFFF' 
           tile='' 
           title='All categories'>
           <i class='fa fa-bars'>
-          </i> #{t :all_categories}
+          </i> <span>#{t :all_categories}</span>
        </button> ".html_safe  
   end
 
