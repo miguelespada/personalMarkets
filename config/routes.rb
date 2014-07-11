7143e2c155d46d36962980dcb0324c2f0b55c609
miguelespada <miguel.valero.espada@gmail.com>
2014-07-11 15:09:40 +0200

    add beta text

--- config/routes.rb
+++ config/routes.rb
@@ -147,6 +147,7 @@ PopUpStores::Application.routes.draw do
     get "static_pages/privacy", as: "privacy"
     get "static_pages/terms", as: "terms"
     get "static_pages/settings", path: "/settings", as: "settings"
+    get "static_pages/beta", path: "/beta", as: "beta"
 
     get "static_pages/home", path: "/", as: 'home'
     root "static_pages#home"
