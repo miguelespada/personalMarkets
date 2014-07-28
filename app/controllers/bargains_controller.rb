3a7aedeb978b21233346ced765096341a95aa7f5
miguelespada <miguel.valero.espada@gmail.com>
2014-07-28 11:45:16 +0200

    some fixes

--- app/controllers/bargains_controller.rb
+++ app/controllers/bargains_controller.rb
@@ -1,6 +1,6 @@
 class BargainsController < ApplicationController
   load_resource :only => [:show, :edit, :destroy, :update]
-  authorize_resource :except => [:show, :list_user_bargains, :gallery]
+  authorize_resource :except => [:show, :list_user_bargains, :gallery_user_bargains, :gallery]
 
   def index
     @bargains = Bargain.all.desc(:created_at)
