af3f5bd44b8cf467d1aa54229d43905a429d7a9c
miguelespada <miguel.valero.espada@gmail.com>
2014-08-06 11:15:09 +0200

    add more internationalization

--- app/domain/controller_notice.rb
+++ app/domain/controller_notice.rb
@@ -1,7 +1,7 @@
 class ControllerNotice
   def self.success action, object
-     s = I18n.t(:your) + " " + I18n.t(object.to_s).downcase + " " + I18n.t(:was) + " " + I18n.t(action.to_s) + " " + I18n.t(:successfully)
-     s.strip
+    s = I18n.t(:your) + " " + I18n.t(object.to_s).downcase + " " + I18n.t(:was) + " " + I18n.t(action.to_s) + " " + I18n.t(:successfully)
+    s.strip
   end
 
   def self.fail action, object
--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -8,7 +8,7 @@ module MarketsHelper
   end
 
   def save_and_publish_button(form, market)
-    form.button :submit, I18n.t("market_form.update_and_publish_market").titleize, class: 'btn btn-info', id: 'save-and-publish-button'
+    form.button :submit, I18n.t("market_form.update_and_publish_market"), class: 'btn btn-info', id: 'save-and-publish-button'
   end
 
   def market_calendar(market)
