e92e8282f709cfe5b904a5f993f5868acf3a82c6
miguelespada <miguel.valero.espada@gmail.com>
2014-09-10 13:07:25 +0200

    some small changes

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module PopUpStores
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
     config.i18n.enforce_available_locales = false
 
     social_keys = File.join(Rails.root, 'config', 'social_keys.yml')
