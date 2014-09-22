2056b798212ffb0a30d20eee441eed27035763ce
jameshedaweng <heda.weng@gmail.com>
2014-09-22 16:51:17 +0200

    Add locator pdf

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -84,7 +84,21 @@ class CouponsController < ApplicationController
   def localizador
     @transaction = CouponTransaction.find(params[:transaction_id])
     authorize! :see_localizador, @transaction
-    render  "coupons/transactions/localizador"
+    respond_to do |format|
+      format.html do
+        render  "coupons/transactions/localizador"
+      end
+      format.pdf do
+        render :pdf => "localizador.pdf", 
+               :template => 'coupons/transactions/localizador_pdf.html.erb',
+               :page_size => "A4",
+               :encoding => "utf8",
+               :margin => {:top    => 5,
+                           :bottom => 5,
+                           :left   => 5,
+                           :right  => 5}
+      end
+    end
   end
 
   private
